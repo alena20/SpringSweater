881c50d28fc31a9417e6572b065cd78df863a010
alena20 <lenanovik98@gmail.com>
2022-01-24 23:58:01 +0300

    role differentiation

--- src/main/java/by/bsuir/sweater/controller/MainController.java
+++ src/main/java/by/bsuir/sweater/controller/MainController.java
@@ -1,9 +1,12 @@
 package by.bsuir.sweater.controller;
 
 import by.bsuir.sweater.domain.Message;
+import by.bsuir.sweater.domain.User;
 import by.bsuir.sweater.repos.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,30 +25,28 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map<String, Object> model){
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model){
         Iterable<Message> messages = messageRepository.findAll();
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepository.findByTag(filter);
+        } else {
+            messages = messageRepository.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
         return "main";
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model){
-        Message message = new Message(text,tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
         messageRepository.save(message);
         Iterable<Message> messages = messageRepository.findAll();
         model.put("messages", messages);
         return "main";
     }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map<String, Object> model) {
-        Iterable<Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.put("messages", messages);
-        return "main";
-    }
 }
--- src/main/java/by/bsuir/sweater/domain/Message.java
+++ src/main/java/by/bsuir/sweater/domain/Message.java
@@ -1,9 +1,6 @@
 package by.bsuir.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -14,17 +11,22 @@ public class Message {
 
     private String text, tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User author;
 
     public Message() {
     }
 
     public Message(String text, String tag, User user) {
+        this.author = user;
         this.text = text;
         this.tag = tag;
-        this.author = author;
     }
 
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
     public Integer getId() {
         return id;
     }
