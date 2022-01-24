4951a2b43374dca4825fb38ed0a9c16f7a72d54d
alena20 <lenanovik98@gmail.com>
2022-01-24 20:53:04 +0300

    Initial commit

---
+++ src/main/java/by/bsuir/sweater/controller/RegistrationController.java
@@ -0,0 +1,38 @@
+package by.bsuir.sweater.controller;
+
+import by.bsuir.sweater.domain.Role;
+import by.bsuir.sweater.domain.User;
+import by.bsuir.sweater.repos.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.persistence.CollectionTable;
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/registration")
+    public String registration(){
+        return "registrarion";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model){
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if(userFromDb != null){
+            model.put("message", "User exists!");
+            return "registration";
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+        return "redirect:/login";
+    }
+}
---
+++ src/main/java/by/bsuir/sweater/domain/Role.java
@@ -0,0 +1,13 @@
+package by.bsuir.sweater.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+
+public enum Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
